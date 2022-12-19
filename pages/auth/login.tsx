b78f4d0436e649d06650372317e403a68142a9a7
ipeq32 <tnhnipek@gmail.com>
2022-12-19 23:05:42 +0300

    added handleSubmit

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -3,7 +3,7 @@ import { Button, Center, Container, Group, TextInput } from "@mantine/core";
 import AuthLayout from "../../components/layout/AuthLayout";
 import Link from "next/link";
 import React from "react";
-import Thirteen from "../../public/next.svg";
+import { signIn } from "next-auth/react";
 import { useForm } from "@mantine/form";
 
 type Props = {};
@@ -16,9 +16,21 @@ const login = (props: Props) => {
     },
 
     validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Geçersiz e-posta"),
+      password: (value) => (value.length >= 6 ? null : "Şifre çok kısa"),
     },
   });
+
+  const handleSubmit = () => {
+    console.log("form valueleri", form.values);
+    signIn("credentials", {
+      redirect: false,
+      email: form.values.email,
+      password: form.values.password,
+    }).then((res) => {
+      console.log("response değeri", res);
+    });
+  };
 
   return (
     <AuthLayout>
@@ -29,6 +41,7 @@ const login = (props: Props) => {
           flex: "flex",
           marginTop: "-180px",
           flexDirection: "column",
+          boxShadow: "50px 50px 100px 0px rgba(0,0,0,0.75)",
         }}
       >
         <h1
@@ -48,13 +61,22 @@ const login = (props: Props) => {
             backgroundImage: `linear-gradient(rgba(57,35,100,.4), rgba(33,40,39,.8))`,
             height: "max-content",
             padding: "20px",
-            borderRadius: "50px",
+            borderRadius: "35px",
+            margin: "50px",
           }}
         >
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
             <TextInput
               size="md"
-              label=<p style={{ color: "white", padding: "0", margin: "0" }}>
+              label=<p
+                style={{
+                  color: "white",
+                  padding: "0",
+                  margin: "0",
+                  pointerEvents: "none",
+                  userSelect: "none",
+                }}
+              >
                 Email
               </p>
               placeholder="enter email"
@@ -63,7 +85,15 @@ const login = (props: Props) => {
             <TextInput
               size="md"
               mt={10}
-              label=<p style={{ color: "white", padding: "0", margin: "0" }}>
+              label=<p
+                style={{
+                  color: "white",
+                  padding: "0",
+                  margin: "0",
+                  pointerEvents: "none",
+                  userSelect: "none",
+                }}
+              >
                 Password
               </p>
               placeholder="enter password"
