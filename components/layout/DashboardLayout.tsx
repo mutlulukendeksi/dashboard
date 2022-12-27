bdaccd3ca5fe48237ed1e79ca2126fcb8bd5b066
Halil Safa SAĞLIK <safa@rubiklabs.com>
2022-12-27 13:55:51 +0300

    Fixed mantine Provider

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -4,7 +4,6 @@ import {
   Burger,
   Container,
   Header,
-  MantineProvider,
   MediaQuery,
   Menu,
   NavLink,
@@ -50,168 +49,159 @@ const DashboardLayout = (props: Props) => {
       <Text>Loading</Text>
     </Loading>
   ) : (
-    <MantineProvider
-      theme={{
-        // Override any other properties from default theme
-        fontFamily: "Chakra Petch, sans serif",
-        colorScheme: "dark",
+    <AppShell
+      layout="alt"
+      styles={{
+        main: {
+          backgroundColor: theme.colors.gray[7],
+        },
       }}
-      withNormalizeCSS
-    >
-      <AppShell
-        layout="alt"
-        styles={{
-          main: {
-            backgroundColor: theme.colors.gray[7],
-          },
-        }}
-        navbarOffsetBreakpoint="sm"
-        asideOffsetBreakpoint="sm"
-        navbar={
-          <Navbar
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          style={{
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "30px",
+            backgroundColor: theme.colors.gray[8],
+          }}
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <NavLink
+            component="a"
+            href="/"
+            label="Dashboard"
+            icon={<IconHome2 size={24} stroke={2} />}
             style={{
-              alignItems: "center",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              gap: "30px",
-              backgroundColor: theme.colors.gray[8],
+              color: "pink",
+              padding: "15px",
+              borderRadius: "15px",
+              textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
+              boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
+              borderRight: "5px solid red",
+              borderLeft: "5px solid red",
+              borderTop: "1px solid pink",
+              borderBottom: "1px solid gray",
             }}
-            p="md"
-            hiddenBreakpoint="sm"
-            hidden={!opened}
-            width={{ sm: 200, lg: 300 }}
+          />
+          <NavLink
+            component="a"
+            href="/user"
+            label="User List"
+            icon={<IconUserCircle size={24} stroke={2} />}
+            style={{
+              color: "pink",
+              padding: "15px",
+              borderRadius: "15px",
+              textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
+              boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
+              borderRight: "5px solid yellow",
+              borderLeft: "5px solid yellow",
+              borderTop: "1px solid orange",
+              borderBottom: "1px solid pink",
+            }}
+          />
+          <NavLink
+            component="a"
+            href="/device"
+            label="Device List"
+            icon={<IconDeviceCameraPhone size={24} stroke={2} />}
+            style={{
+              color: "pink",
+              padding: "15px",
+              borderRadius: "15px",
+              textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
+              boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
+              borderRight: "5px solid pink",
+              borderLeft: "5px solid pink",
+              borderTop: "1px solid gray",
+              borderBottom: "1px solid white",
+            }}
+          />
+          <NavLink
+            component="a"
+            href="/emotion"
+            label="Emotion List"
+            icon={<IconHeart size={24} stroke={2} />}
+            style={{
+              color: "pink",
+              padding: "15px",
+              borderRadius: "15px",
+              textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
+              boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
+              borderRight: "5px solid purple",
+              borderLeft: "5px solid purple",
+              borderTop: "1px solid gray",
+              borderBottom: "1px solid pink",
+            }}
+          />
+        </Navbar>
+      }
+      header={
+        <Header
+          height={{ base: 50, md: 70 }}
+          p="md"
+          style={{ backgroundColor: theme.colors.gray[8] }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              height: "100%",
+            }}
           >
-            <NavLink
-              component="a"
-              href="/"
-              label="Dashboard"
-              icon={<IconHome2 size={24} stroke={2} />}
-              style={{
-                color: "pink",
-                padding: "15px",
-                borderRadius: "15px",
-                textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
-                boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
-                borderRight: "5px solid red",
-                borderLeft: "5px solid red",
-                borderTop: "1px solid pink",
-                borderBottom: "1px solid gray",
-              }}
-            />
-            <NavLink
-              component="a"
-              href="/user"
-              label="User List"
-              icon={<IconUserCircle size={24} stroke={2} />}
-              style={{
-                color: "pink",
-                padding: "15px",
-                borderRadius: "15px",
-                textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
-                boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
-                borderRight: "5px solid yellow",
-                borderLeft: "5px solid yellow",
-                borderTop: "1px solid orange",
-                borderBottom: "1px solid pink",
-              }}
-            />
-            <NavLink
-              component="a"
-              href="/device"
-              label="Device List"
-              icon={<IconDeviceCameraPhone size={24} stroke={2} />}
-              style={{
-                color: "pink",
-                padding: "15px",
-                borderRadius: "15px",
-                textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
-                boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
-                borderRight: "5px solid pink",
-                borderLeft: "5px solid pink",
-                borderTop: "1px solid gray",
-                borderBottom: "1px solid white",
-              }}
-            />
-            <NavLink
-              component="a"
-              href="/emotion"
-              label="Emotion List"
-              icon={<IconHeart size={24} stroke={2} />}
-              style={{
-                color: "pink",
-                padding: "15px",
-                borderRadius: "15px",
-                textShadow: "0px 0px 10px rgba(255,255,255,0.5)",
-                boxShadow: "0px 0px 10px rgba(255,255,255,0.7)",
-                borderRight: "5px solid purple",
-                borderLeft: "5px solid purple",
-                borderTop: "1px solid gray",
-                borderBottom: "1px solid pink",
-              }}
-            />
-          </Navbar>
-        }
-        header={
-          <Header
-            height={{ base: 50, md: 70 }}
-            p="md"
-            style={{ backgroundColor: theme.colors.gray[8] }}
-          >
-            <div
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Container
+              fluid
+              w="100%"
               style={{
                 display: "flex",
-                alignItems: "center",
-                height: "100%",
+                justifyContent: "space-between",
               }}
             >
-              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-                <Burger
-                  opened={opened}
-                  onClick={() => setOpened((o) => !o)}
-                  size="sm"
-                  color={theme.colors.gray[6]}
-                  mr="xl"
-                />
-              </MediaQuery>
+              <div></div>
+              <Menu shadow="md" width={200}>
+                <Menu.Target>
+                  <Avatar radius="xl" style={{ cursor: "pointer" }} />
+                </Menu.Target>
 
-              <Container
-                fluid
-                w="100%"
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
-                <div></div>
-                <Menu shadow="md" width={200}>
-                  <Menu.Target>
-                    <Avatar radius="xl" style={{ cursor: "pointer" }} />
-                  </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item icon={<IconSettings size={14} />}>
+                    Settings
+                  </Menu.Item>
 
-                  <Menu.Dropdown>
-                    <Menu.Item icon={<IconSettings size={14} />}>
-                      Settings
-                    </Menu.Item>
-
-                    <Menu.Divider />
-                    <Menu.Item
-                      onClick={() => signOut()}
-                      color="red"
-                      icon={<IconLogout size={14} />}
-                    >
-                      Sign Out
-                    </Menu.Item>
-                  </Menu.Dropdown>
-                </Menu>
-              </Container>
-            </div>
-          </Header>
-        }
-      >
-        {props.children}
-      </AppShell>
-    </MantineProvider>
+                  <Menu.Divider />
+                  <Menu.Item
+                    onClick={() => signOut()}
+                    color="red"
+                    icon={<IconLogout size={14} />}
+                  >
+                    Sign Out
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Container>
+          </div>
+        </Header>
+      }
+    >
+      {props.children}
+    </AppShell>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import { SessionProvider } from "next-auth/react";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <MantineProvider
+        theme={{
+          // Override any other properties from default theme
+          fontFamily: "Chakra Petch, sans serif",
+          colorScheme: "dark",
+        }}
+        withNormalizeCSS
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   )
 }
