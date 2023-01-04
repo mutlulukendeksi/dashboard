2f868915981e36588848f82c1fbebc4f7006f860
ipeq32 <tnhnipek@gmail.com>
2023-01-04 21:15:00 +0300

    added change password tab

---
+++ components/Settings/Security.tsx
@@ -0,0 +1,82 @@
+﻿import { Box, Button, Group, Notification, TextInput } from "@mantine/core";
+
+import React from "react";
+import axios from "axios";
+import { useForm } from "@mantine/form";
+import {toast} from "react-toastify"
+
+type Props = {};
+
+const Security = (props: Props) => {
+  const form = useForm({
+    initialValues: {
+      password: "",
+      newPassword: "",
+      newPasswordConfirmation: "",
+    },
+
+    validate: {
+      password: (value) => (value.length >= 6 ? null : "Şifre çok kısa"),
+      newPassword: (value) => (value.length >= 6 ? null : "Şifre çok kısa"),
+      newPasswordConfirmation: (value) =>
+        value === form.values.newPassword ? "" : "Şifreler eşleşmiyor",
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      await axios.post(`/api/user/change_pass`, {
+        data: {
+          oldPassword: form.values.password,
+          password: form.values.newPassword,
+          passwordc: form.values.newPasswordConfirmation,
+        },
+      })
+      toast.success("Şifreniz başarıyla değiştirildi")
+      form.values.password = ""
+      form.values.newPassword = ""
+      form.values.newPasswordConfirmation = ""
+    }catch(err){
+      console.log("datası hata",err.response.data)
+      toast.error(err.response.data.error)
+    }
+  };
+
+  return (
+    <Box sx={{ maxWidth: 300, marginTop: 30 }} mx="auto">
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          withAsterisk
+          type="password"
+          label="Password"
+          placeholder="eski şifreniz"
+          {...form.getInputProps("password")}
+          style={{ marginBottom: 20 }}
+        />
+        <TextInput
+          withAsterisk
+          type="password"
+          label="New password"
+          placeholder="yeni şifreniz"
+          {...form.getInputProps("newPassword")}
+          style={{ marginBottom: 20 }}
+        />
+        <TextInput
+          withAsterisk
+          type="password"
+          label="New password confirmation"
+          placeholder="yeni şifreniz"
+          {...form.getInputProps("newPasswordConfirmation")}
+          style={{ marginBottom: 20 }}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Kaydet</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+};
+
+export default Security;
---
+++ pages/api/user/change_pass.tsx
@@ -0,0 +1,47 @@
+﻿import type { NextApiRequest, NextApiResponse } from "next";
+
+import bcrypt from "bcryptjs";
+import { getSession } from "next-auth/react";
+import prisma from "../../../lib/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
+  const userEmail = session.user.email;
+  const userData = req.body.data;
+  console.log("user datası burda", userData);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: userEmail,
+    }
+  });
+  // console.log("USERR",user)
+
+  const passControl = userData.password === userData.passwordc;
+  if (!passControl) {
+    res.status(400).json({ error: "Şifreler eşleşmiyor" });
+    return;
+  }
+
+  const validPassword = await bcrypt.compare(userData.oldPassword, user.password);
+  if (!validPassword) {
+    res.status(400).json({ error: "eski Şifreniz yanlış" });
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+  const userUpdate = await prisma.user.update({
+    where: {
+      email: userEmail,
+    },
+    data: {
+      password: hashedPassword,
+    },
+  });
+  res.status(200).json({ userUpdate });
+}
